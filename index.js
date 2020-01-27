e9569c27422fb0b54af85595b30485e374e2586c
argush3 <argus@highwaythreesolutions.com>
2020-01-27 12:42:32 -0800

    Update action

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const matchAll = require("match-all");
 
-try {
-    const commitMessage = core.getInput('commit-message');
-    console.log("commitMessage: " + commitMessage);
 
-    const matches = matchAll(commitMessage, /([A-Z]+-\d+)/g).toArray();
-    const result = matches.join(',');
-    console.log("result: ", result);
+async function extractJiraKeysFromCommit() {
+    try {
+        const commitMessage = core.getInput('commit-message');
+        console.log("commitMessage: " + commitMessage);
 
-    core.setOutput("jira-keys", result);
-} catch (error) {
-    core.setFailed(error.message);
+        const matches = matchAll(commitMessage, /([A-Z]+-\d+)/g).toArray();
+        const result = matches.join(',');
+        console.log("result: ", result);
+
+        core.setOutput("jira-keys", result);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
+
+(async function () {
+    await extractJiraKeysFromCommit();
+    console.log("finished extracting jira keys from commit message");
+})();

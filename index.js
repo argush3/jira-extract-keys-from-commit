caeac66fc0b46204461714183b87b1e8559dd7b0
Argus Chiu <argus@highwaythreesolutions.com>
2020-02-13 06:46:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function extractJiraKeysFromCommit() {
         // console.log("github context json payload: ", jsonPayload);
         const payload = github.context.payload;
         console.log("github: ", github);
-        console.log("github.run_id: ", github.run_id);
+        console.log("GITHUB_RUN_ID: ", process.env['GITHUB_RUN_ID']);
 
         const token = process.env['GITHUB_TOKEN'];
         console.log("github token: " + token);
